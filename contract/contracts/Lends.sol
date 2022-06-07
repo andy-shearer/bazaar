519f7c42c9a3653e44b14c3d626abd5a4ca98e59
Andy Shearer <andyshearer.95@googlemail.com>
2022-06-07 15:47:30 +0100

    Add function for borrower to send the funds to the contract
    The borrow fee and the collateral is sent in one call

--- contract/contracts/Lends.sol
+++ contract/contracts/Lends.sol
@@ -7,21 +7,28 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract Lends is Ownable, Pausable {
     uint256 public agreementCount;
+    uint256 public totalStaked; // make private?
     mapping(uint256 => LendAgreement) public lends;
 
     struct LendAgreement {
         address lender;
         address borrower;
         bool borrowerApproval;
+        bool borrowerHasFunded;
         bool lenderApproval;
         bool started;
-        uint256 borrowDur; // Stored in days
+        uint256 borrowDur; // stored in days
         uint256 borrowFee;
         uint256 collateral;
         uint256 endDate;
+        uint256 remainingStake; // tracks the stake that is stored in the contract
+        uint256 remainingFee; // tracks the fee that is stored in the contract
     }
 
-    event NewLend(LendAgreement agreement);
+    event NewLend(address lender, address borrower, uint256 borrowDur);
+    event LendStarted(uint256 id, address lender, address borrower, uint256 endDate);
+    event FeeWithdraw(address claimant, uint256 amount);
+    event StakeWithdraw(address claimant, uint256 amount);
 
     modifier lendAgreementExists(uint256 _lendId) {
         require(bytes(lends[_lendId]).length > 0, "Lend Agreement does not exist");
@@ -38,6 +45,7 @@ contract Lends is Ownable, Pausable {
 
     constructor() {
         agreementCount = 0;
+        totalStaked = 0;
     }
 
     function createLendAgreement(
@@ -51,6 +59,9 @@ contract Lends is Ownable, Pausable {
         onlyWhenNotPaused
         returns (uint256 newLendId)
     {
+        require(_lender != address(0), "Invalid lender address");
+        require(_borrower != address(0), "Invalid borrower address");
+
         newLendId = ++agreementCount;
         LendAgreement storage lend = lends[newLendId];
         lend.lender = _lender;
@@ -58,6 +69,7 @@ contract Lends is Ownable, Pausable {
         lend.borrowDur = _dur;
         lend.borrowFee = _fee;
         lend.collateral = _collateral;
+        emit NewLend(_lender, _borrower, _dur);
     }
 
     function approveLendAgreement(uint256 _lendId)
@@ -67,11 +79,27 @@ contract Lends is Ownable, Pausable {
     {
         LendAgreement storage lend = lends[_lendId];
 
-        if(isLender) {
+        if(lend.lender == msg.sender) {
             lend.lenderApproval = true;
-        } else if(isBorrower) {
+        } else if(lend.borrower == msg.sender) {
             lend.borrowerApproval = true;
         }
+    }
+
+    function sendFunds(uint256 _lendId)
+        public
+        payable
+        onlyWhenNotPaused
+        lendAgreementExists(_lendId)
+    {
+        LendAgreement storage lend = lends[_lendId];
+        require(lend.borrower == msg.sender, "You are not the borrower for this Lend Agreement");
+        require(msg.value >= (lend.borrowFee + lend.collateral), "Not enough funds sent");
+
+        lend.remainingFee = lend.borrowFee;
+        lend.remainingStake = lend.collateral;
+        lend.borrowerHasFunded = true;
+        totalStaked += lend.collateral;
     }
 
     function startLend(uint256 _lendId)
@@ -83,9 +111,16 @@ contract Lends is Ownable, Pausable {
 
         require(lend.lenderApproval, "Lender has not approved this Lend Agreement");
         require(lend.borrowerApproval, "Borrower has not approved this Lend Agreement");
+        require(lend.borrowerHasFunded, "Borrower has not yet sent the agreed funds for this Lend Agreement");
         require(lend.lender == msg.sender, "Only the lender can start lending");
 
         lend.endDate = now + (lend.borrowDur * 1 days);
         lend.started = true;
+
+        emit LendStarted(_lendId, lend.lender, lend.borrower, lend.endDate);
     }
+    
+    // Set up default functions for the contract
+    receive() external payable {}
+    fallback() external payable {}
 }
