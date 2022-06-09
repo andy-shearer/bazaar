28c149a0dcb7035437105d05bc3d316a93d6deb1
Andy Shearer <andyshearer.95@googlemail.com>
2022-06-09 11:23:47 +0100

    WIP site bits

---
+++ site/components/Footer.js
@@ -0,0 +1,37 @@
+import React from "react"
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://github.com/andy-shearer"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.a}
+      >
+        Built by Andy Shearer
+      </a>
+      <section className={styles.footerLinks}>
+        <a href="https://twitter.com/devshez" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/twitter.svg"
+            width="20px"
+            height="20px"
+          />
+          devshez
+        </a>
+        <br />
+        <a href="https://twitter.com/TrustlessBazaar" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/twitter.svg"
+            width="20px"
+            height="20px"
+          />
+          TrustlessBazaar
+        </a>
+      </section>
+    </footer>
+  );
+}
