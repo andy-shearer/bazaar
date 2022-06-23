7c6264b7d5b0da561ac4c92f3a9776d1b7cbb546
Andy Shearer <andyshearer.95@googlemail.com>
2022-06-23 16:20:16 +0100

    Info boxes

--- site/pages/index.js
+++ site/pages/index.js
@@ -41,14 +41,59 @@ export default function Home() {
         </p>
       </section>
 
-      <img
-        src="/homepage.jpg"
-        alt="Image of three market stalls of vendors selling various items"
-        title="Bazaar vector created by pikisuperstar - www.freepik.com"
-        className={styles.homepageGraphic}
-      />
+      <div className={styles.detailContainer}>
+        <div className={styles.detailText}>
+          <h2 className={styles.detailHeader}>
+            Borrow More, Buy Less
+          </h2>
+          <p>
+            A lot of people own things that they rarely use. Borrow these things from people in your local area.<br/><br/>
+            Borrowing and lending is secured by blockchain smart contracts. Lend agreements are negotiated by both sides and the agreement is signed using the lender and borrower&apos;s digital signatures.
+          </p>
+        </div>
+        <img
+          src="/market_1.png"
+          alt="Market stall vendor selling various items"
+          title="Bazaar vector created by pikisuperstar - www.freepik.com"
+          className={classNames(styles.detailGraphic, styles.graphicRight)}
+        />
 
-      {/*<main className={styles.main}>
+        <img
+          src="/market_2.png"
+          alt="Market stall vendor selling various items"
+          title="Bazaar vector created by pikisuperstar - www.freepik.com"
+          className={styles.detailGraphic}
+        />
+
+        <div className={classNames(styles.detailText, styles.textRight)}>
+          <h2 className={styles.detailHeader}>
+            Collateralised Borrowing
+          </h2>
+          <p>
+            The lender of the item can set the contingency value to the value of the item that they are lending.
+            This value is securely held in the platform by the borrower whilst the item is being borrowed.<br/><br/>
+            If the item isn&apos;t returned according to the terms of the agreement, the lender can claim the contingency value that the borrower provided.
+          </p>
+        </div>
+      </div>
+
+      {/*
+      <div className={styles.infoContainer}>
+        <h2 className={styles.infoHeader}>
+
+        </h2>
+        <p className={styles.infoText}>
+
+        </p>
+        <img
+          src="/market_3.png"
+          alt="Market stall vendor selling various items"
+          title="Bazaar vector created by pikisuperstar - www.freepik.com"
+          className={styles.homepageGraphic}
+        />
+      </div>
+
+      <main className={styles.main}>
         <h2 className={styles.title}>
           Borrow Requests
         </h2>
@@ -60,9 +105,9 @@ export default function Home() {
         </h2>
 
         <AvailableBorrows />
-      </main>*/
+      </main>*/}
 
-      <Footer />}
+      <Footer />
     </div>
   )
 }
