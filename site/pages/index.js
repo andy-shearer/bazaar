7751d7bbb3f213d03e749b5b86853987395d3674
Andy Shearer <andyshearer.95@googlemail.com>
2022-06-23 13:55:46 +0100

    Revert "WIP parallax bits"
    This reverts commit f1ba910cec7ff4923906c3e487eccc1783266366.

--- site/pages/index.js
+++ site/pages/index.js
@@ -30,42 +30,25 @@ export default function Home() {
         </button>
       </section>
 
-      <div id="wrapper" className={styles.wrapper}>
-        <div id="parallaxContainer" className={styles.parallax_container}>
-          <div id="backgroundImgContainer" className={styles.background}>
-            <img src="/homepage.jpg" alt="Market vendors"/>
-          </div>
-          <div id="foregroundContainer" className={styles.foreground}>
-            <div id="infoText" className={styles.infoText}>
-              <p className={styles.infoTextHeading}>
-                Borrow the things<br/>
-                you don&apos;t need to buy.
-              </p>
-              <p className={styles.infoTextSub}>
-                A secure way for strangers to lend & borrow items.<br/>
-                Secured by blockchain smart contracts.
-              </p>
+      <section className={styles.infoText}>
+        <p className={styles.infoTextHeading}>
+          Borrow the things<br/>
+          you don&apos;t need to buy.
+        </p>
+        <p className={styles.infoTextSub}>
+          A secure way for strangers to lend & borrow items.<br/>
+          Secured by blockchain smart contracts.
+        </p>
+      </section>
 
-              <h2>Collateralised Borrowing</h2>
-              <p>
-                The lender of the item can set the contingency value to the value of the item that they are lending.<br/><br/>
-                This value is securely held in the platform by the borrower whilst the item is being borrowed.<br/>
-                If the item isn&apos;t returned according to the terms of the agreement, the lender can claim the contingency value that the borrower provided.
-              </p>
+      <img
+        src="/homepage.jpg"
+        alt="Image of three market stalls of vendors selling various items"
+        title="Bazaar vector created by pikisuperstar - www.freepik.com"
+        className={styles.homepageGraphic}
+      />
 
-              <h2>Borrow More, Buy Less</h2>
-              <p>
-              A lot of people own things that they rarely use. Borrow these things from people in your local area.<br/><br/>
-              Borrowing and lending is secured by blockchain smart contracts.<br/>
-              Lend agreements are negotiated by both sides and the agreement is signed using the lender and borrower’s digital signatures.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/*
-      <main className={styles.main}>
+      {/*<main className={styles.main}>
         <h2 className={styles.title}>
           Borrow Requests
         </h2>
@@ -77,10 +60,9 @@ export default function Home() {
         </h2>
 
         <AvailableBorrows />
-      </main>
-      */}
+      </main>*/
 
-      <Footer />
+      <Footer />}
     </div>
   )
 }
